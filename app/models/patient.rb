7ed4cc0d4201844e0bfeea665eb23f5edde54cf0
timothykiyeng <timothy.kiyeng@student.moringaschool.com>
2022-12-22 09:12:51 +0300

    initial commit

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -4,7 +4,15 @@ class Patient < ApplicationRecord
     has_many :medical_histories
     has_secure_password
 
+    PASSWORD_REQUIREMENTS = /\A
+        (?=.{8,}) #At least eight characters long
+        (?=.*\d)  #Contain at least one number
+        (?=.*[a-z]) #contain at least one lowercase letter
+        (?=.*[A-Z]) #contain at least one upercase letter
+        (?=*[[:^alnum:]]) #contain at least one symbol
+    /x
 
     validates :email, presence: true, uniqueness: true
     validates :name, presence: true
+    validates :password, format: PASSWORD_REQUIREMENTS
 end
