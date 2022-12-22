bd670532ec85ee12c8805795211467b50279b5a4
timothykiyeng <timothy.kiyeng@student.moringaschool.com>
2022-12-22 09:12:51 +0300

    initial commit

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -4,14 +4,15 @@ class Doctor < ApplicationRecord
     has_many :patients, through: :appointments
     has_secure_password
 
-
-    validates :email, presence: true, uniqueness: true
-    validates :name, presence: true
-    validate :permitted_email
-
     def permitted_email
         unless email.match?(/medizone.com/)
         errors.add(:permitted_emails, ": Must have a Medical Zone email.")
         end
     end
+
+
+    validates :email, presence: true, uniqueness: true
+    validates :name, presence: true
+    validate :permitted_email
+
 end
--- app/models/patient.rb
+++ app/models/patient.rb
@@ -14,5 +14,7 @@ class Patient < ApplicationRecord
 
     validates :email, presence: true, uniqueness: true
     validates :name, presence: true
+    validates :password, confirmation: true
+    validates :password_confirmation, presence: true
     validates :password, format: PASSWORD_REQUIREMENTS
 end
