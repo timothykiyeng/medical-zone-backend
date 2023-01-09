40c9bbc3d6bc8642b74dfa7ec786196775f1a7ad
timothykiyeng <timothy.kiyeng@student.moringaschool.com>
2023-01-09 15:49:47 +0300

    created patients session and added login/signup routes

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,6 @@ class SessionsController < ApplicationController
     patient=Patient.find_by(username:params[:username])
     if patient&.authenticate(params[:password])
         session[:user_id]=patient.id
-        # render json: subscriber
         render json: {success:"Welcome #{patient.username}"}
     else
         render json:{error:"Invalid username or password"}, status: :unauthorized
