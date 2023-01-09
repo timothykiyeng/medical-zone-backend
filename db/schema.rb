5e5662f4d69073a35bab5f92897beec3c60797e6
diana nthenya odhiambo <dianaodhiambo1096@gmail.com>
2023-01-09 20:27:51 +0300

    changes age datatype  to integer

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_09_052331) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_09_152900) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -54,7 +54,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_09_052331) do
 
   create_table "patients", force: :cascade do |t|
     t.string "name"
-    t.string "age"
+    t.integer "age"
     t.integer "birth_date"
     t.string "email"
     t.string "diagnosis"
