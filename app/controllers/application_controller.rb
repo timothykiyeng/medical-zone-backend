d416f6849b8a73efd246c8ee5f7c68753b7b2cc5
timothykiyeng <timothy.kiyeng@student.moringaschool.com>
2023-01-09 15:24:18 +0300

    rescue syntax to rescue out of an exception

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,4 +8,17 @@ class ApplicationController < ActionController::API
     # { Authorization: 'Bearer <token>' }
     request.headers["Authorization"]
   end
+  
+  def decoded_token
+    if auth_header
+      token = auth_header.split(" ").last
+      # header: { 'Authorization': 'Bearer <token>' }
+      begin
+        JWT.decode(token, "my_s3cr3t", true, algorithm: "HS256")
+      rescue JWT::DecodeError
+        nil
+      end
+    end
+  end
+
 end
