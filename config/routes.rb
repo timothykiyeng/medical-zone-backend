48516fbb85654ef6c7cf1781c4d0022348c3857b
diana nthenya odhiambo <dianaodhiambo1096@gmail.com>
2023-01-12 16:20:21 +0300

    adds endpoints for different resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,30 @@
 Rails.application.routes.draw do
-  resources :patients
-  resources :doctors
-  resources :appointments
   resources :medical_histories, only: [:index]
+  resources :appointments
+  resources :doctors, only: [:index, :show]
+  resources :patients, only: [:index, :show, :create, :destroy]
   resources :departments, only: [:index]
-  # display all doctors
-  get "/doctors", to: "doctors#index"
-  # display one doctor
-  get "/doctors/:id", to: "doctors#show"
-  post "/signup",to: "patients#create"
-  get "/patients", to: "patients#index"
-  get "/patients/:id", to: "patients#show"
-  delete "/logout", to: "sessions#destroy"
-  post "/login-patient", to: "jwt#create"
-  post '/signup-doctor', to: 'doctors#create'
-  post "/logindoctor", to: "doctor_jwt#create"
-
+  post '/doclogin', to: 'sessions#doclogin'
+  post '/patientlogin', to: 'sessions#patientlogin'
+  delete '/logout', to: 'sessions#logout'
 end
+
+# Rails.application.routes.draw do
+#   resources :patients
+#   resources :doctors
+#   resources :appointments
+#   resources :medical_histories, only: [:index]
+#   resources :departments, only: [:index]
+#   # display all doctors
+#   get "/doctors", to: "doctors#index"
+#   # display one doctor
+#   get "/doctors/:id", to: "doctors#show"
+#   post "/signup",to: "patients#create"
+#   get "/patients", to: "patients#index"
+#   get "/patients/:id", to: "patients#show"
+#   delete "/logout", to: "sessions#destroy"
+#   post "/login-patient", to: "jwt#create"
+#   post '/signup-doctor', to: 'doctors#create'
+#   post "/logindoctor", to: "doctor_jwt#create"
+
+# end
