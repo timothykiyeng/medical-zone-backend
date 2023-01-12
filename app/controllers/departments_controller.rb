93b5a0e02ad089282f46d3e15ca637f386c3d428
diana nthenya odhiambo <dianaodhiambo1096@gmail.com>
2023-01-12 16:07:14 +0300

    creates index route for department controller

--- app/controllers/departments_controller.rb
+++ app/controllers/departments_controller.rb
@@ -1,4 +1,7 @@
 class DepartmentsController < ApplicationController
+  skip_before_action :authorize
+  skip_before_action :is_doc
+ 
   def index
     render json: Department.all, status: :ok
   end
