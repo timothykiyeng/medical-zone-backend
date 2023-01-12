84b1965a193163674eaa64eb00cec658ce9f447e
diana nthenya odhiambo <dianaodhiambo1096@gmail.com>
2023-01-12 16:17:35 +0300

    filters data to be displayed to the user

--- app/serializers/doctor_serializer.rb
+++ app/serializers/doctor_serializer.rb
@@ -1,3 +1,3 @@
 class DoctorSerializer < ActiveModel::Serializer
-  attributes :id, :title, :name, :bio, :department_id, :patient_id, :email, :password_digest
+  attributes :id, :title, :name, :bio, :email, :doc
 end
