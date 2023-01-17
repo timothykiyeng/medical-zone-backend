41c345eaac07f6f226d0fac577aefe63abe0ac46
timothykiyeng <timothy.kiyeng@student.moringaschool.com>
2023-01-17 19:44:39 +0300

    fixes unaothized bug on creation of a new appointment

--- app/controllers/medical_histories_controller.rb
+++ app/controllers/medical_histories_controller.rb
@@ -1,7 +1,7 @@
 class MedicalHistoriesController < ApplicationController
-  skip_before_action :authorize
-  skip_before_action :is_doc
-  
+  # skip_before_action :authorize
+  # skip_before_action :is_doc
+
   def index
     render json: MedicalHistory.all, status: :ok
   end
