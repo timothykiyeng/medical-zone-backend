ee0f01868e3bbe13588b882977683861a8b3aad1
diana nthenya odhiambo <dianaodhiambo1096@gmail.com>
2023-01-17 22:31:13 +0300

    updates seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,15 +15,15 @@ Department.create(name: 'Vascular', image_url: 'https://www.flaticon.com/free-ic
 Department.create(name: 'Neurology', image_url: 'https://www.flaticon.com/free-icon/neurology_9133531?term=neurology&page=1&position=31&origin=search&related_id=9133531')
 
 puts 'Seeding patients..'
-Patient.create(name: "Ben Anthony", age: rand(18..65), birthdate: Faker::Date.birthday(min_age: 18, max_age: 65), email: "ba1@gmail.com", password: "123", gender:'Male', doc: false)
-Patient.create(name: "Jenna Adams", age: rand(18..65), birthdate: Faker::Date.birthday(min_age: 18, max_age: 65), email: "jenna@gmail.com", password: "124", gender:'Female', doc: false)
+Patient.create(name: "Ben Anthony", age: rand(18..65), birthdate: Faker::Date.birthday(min_age: 18, max_age: 65), email: "ba1@gmail.com", password: "Power1234#", gender:'Male', doc: false)
+Patient.create(name: "Jenna Adams", age: rand(18..65), birthdate: Faker::Date.birthday(min_age: 18, max_age: 65), email: "jenna@gmail.com", password: "Power124#", gender:'Female', doc: false)
 Patient.create(name: "Anna Johns", age: rand(18..65), birthdate: Faker::Date.birthday(min_age: 18, max_age: 65), email: "anna@gmail.com", password: "125", gender:'Female', doc: false)
 Patient.create(name: "Anthony Caleb", age: rand(18..65), birthdate: Faker::Date.birthday(min_age: 18, max_age: 65), email: "ca1@gmail.com", password: "126", gender:'Male', doc: false)
 
 puts 'Seeding doctors..'
 Doctor.create(title: 'MD', name: 'Bruno Rodrigues', bio: 'Bruno Rodrigues is a doctor (MD or DO) who practices anesthesia. Anesthesiologists are physicians specializing in perioperative care, developing anesthetic plans, and the administration of anesthetics.', department_id: 1, patient_id: 1, email: 'bruno@medizone.com', password: '123', image_url:'https://images.unsplash.com/photo-1622253692010-333f2da6031d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1964&q=80', doc: true, is_available: true)
 Doctor.create(title: 'DO', name: 'Mark Moses', bio: 'Mark Moses is an expert in heart and blood vessel diseases. He can treat heart diseases and help keep you from getting heart diseases.', department_id: 2, patient_id: 2, email: 'mark@medizone.com', password: '456', image_url:'https://img.freepik.com/free-photo/portrait-successful-young-doctor-with-folder-stethoscope_1262-12410.jpg?size=626&ext=jpg&ga=GA1.1.1287727828.1673614897', doc: true, is_available: true)
-Doctor.create(title: 'MD', name: 'Diana Adams', bio: 'Diana Adams is an ear, nose, and throat doctor (ENT). She specializes in everything having to do with those parts of the body.', department_id: 3, patient_id: 3, email: 'dianaadams@medizone.com', password: '123', image_url:'https://images.unsplash.com/photo-1643297654416-05795d62e39c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=853&q=80', doc: true, is_available: true)
+Doctor.create(title: 'MD', name: 'Diana Adams', bio: 'Diana Adams is an ear, nose, and throat doctor (ENT). She specializes in everything having to do with those parts of the body.', department_id: 3, patient_id: 3, email: 'dianaadams@medizone.com', password: 'Power123!', image_url:'https://images.unsplash.com/photo-1643297654416-05795d62e39c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=853&q=80', doc: true, is_available: true)
 Doctor.create(title: 'DO', name: 'Mary Peters', bio: 'Mary Peters is a medical practitioner specializing in children and their diseases.', department_id: 4, patient_id: 4, email: 'mary@medizone.com', password: '123', image_url:'https://sokodirectory.com/wp-content/uploads/2021/07/Nurse-e1627632051412.jpg', doc: true, is_available: true)
 Doctor.create(title: 'MD', name: 'Nancy Kunyiha', bio: 'Nancy Kunyiha is a doctor who stabilizes and treats patients who are experiencing acute health problems or traumatic injuries.', department_id: 5, patient_id: 5, email: 'kunyiha@medizone.com', password: '123', image_url:'https://www.freepik.com/free-photo/portrait-african-female-doctor-lab-coat-with-face-mask-stethoscope-against-blue-background_21122812.htm#query=black%20female%20nurse&position=10&from_view=search&track=ais', doc: true, is_available: true)
 Doctor.create(title: 'DO', name: 'Annita Daniels', bio: 'Annita Daniels is a general surgeon that focuses on alimentary canal and abdominal contents including the esophagus, stomach, small intestine, large intestine, liver, pancreas, gallbladder, appendix and bile ducts, and often the thyroid gland.', department_id: 6, patient_id: 6, email: 'annita@medizone.com', password: '123', image_url:'https://cms.thenairobihosp.org/uploads/home_about_hosi_0f702bbc5f.jpg', doc: true, is_available: true)
@@ -38,8 +38,8 @@ puts 'Seeding appointments..'
     doctor_id: 4,
     patient_id: 1,
     title: 'Pediactrics',
-    start_date: DateTime.new(2022,6,rand(1..30),rand(1..19),rand(1.60)),
-    end_date: DateTime.new(2022,6,rand(1..30),rand(6..19),rand(1.60)),
+    start_date: Date.new(2023,1,rand(1..30)),
+    end_date: Date.new(2023,1,rand(1..30)),
     description: 'My daughter has a mysterious rash that wont go away'
   )
 
@@ -47,8 +47,8 @@ puts 'Seeding appointments..'
     doctor_id: 7,
     patient_id: 2,
     title: 'Hematology',
-    start_date: DateTime.new(2022,6,rand(1..30),rand(1..19),rand(1.60)),
-    end_date: DateTime.new(2022,6,rand(1..30),rand(6..19),rand(1.60)),
+    start_date: Date.new(2023,1,rand(1..30)),
+    end_date: Date.new(2023,1,rand(1..30)),
     description: 'My blood takes a lot of time to clot'
   )
 
@@ -56,8 +56,8 @@ puts 'Seeding appointments..'
     doctor_id: 8,
     patient_id: 3,
     title: 'Oncology',
-    start_date: DateTime.new(2022,6,rand(1..30),rand(1..19),rand(1.60)),
-    end_date: DateTime.new(2022,6,rand(1..30),rand(6..19),rand(1.60)),
+    start_date: Date.new(2023,1,rand(1..30)),
+    end_date: Date.new(2023,1,rand(1..30)),
     description: 'I am experiencing shortness of breath, headaches, and diziness'
   )
 
@@ -65,35 +65,35 @@ puts 'Seeding appointments..'
     doctor_id: 10,
     patient_id: 4,
     title: 'Neurology',
-    start_date: DateTime.new(2022,6,rand(1..30),rand(1..19),rand(1.60)),
-    end_date: DateTime.new(2022,6,rand(1..30),rand(6..19),rand(1.60)),
+    start_date: Date.new(2023,2,rand(1..30)),
+    end_date: Date.new(2023,2,rand(1..30)),
     description: 'Suffering from Weakness or loss of muscle strength'
-  ) 
+  )
 
 
 puts 'Seeding results..'
     MedicalHistory.create(
     diagnosis: 'Anemia: This involves the body producing too few healthy red blood cells to carry enough oxygen around the body.',
     patient_id: 2,
-    test_date: DateTime.new(2022,6,rand(1..30),rand(1..19),rand(1.60))
+    test_date: Date.new(2022,12,rand(1..30))
   )
 
   MedicalHistory.create(
     diagnosis: 'Chickenpox: Viral rashes in babies, toddlers and kids are caused by a variety of different viruses.',
     patient_id: 1,
-    test_date: DateTime.new(2022,6,rand(1..30),rand(1..19),rand(1.60))
+    test_date: Date.new(2022,12,rand(1..30))
   )
 
   MedicalHistory.create(
     diagnosis: 'Bone Caner: Bone scans are a type of nuclear scan that check for abnormal areas or damage in the bones.',
     patient_id: 3,
-    test_date: DateTime.new(2022,6,rand(1..30),rand(1..19),rand(1.60))
+    test_date: Date.new(2022,12,rand(1..30))
   )
 
   MedicalHistory.create(
     diagnosis: 'Epilepsy',
     patient_id: 4,
-    test_date: DateTime.new(2022,6,rand(1..30),rand(1..19),rand(1.60))
+    test_date: Date.new(2022,12,rand(1..30))
   )
 puts "🌱 Seeding done..."
 
