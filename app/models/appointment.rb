0cf30fecb6ae6cfcb0dc80aa88dc009c59ab9679
timothykiyeng <timothy.kiyeng@student.moringaschool.com>
2023-01-29 21:50:47 +0300

    add optional validation to belongs_to models

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -1,6 +1,6 @@
 class Appointment < ApplicationRecord
-  belongs_to :patient
-  belongs_to :doctor
+  belongs_to :patient, optional: true
+  belongs_to :doctor, optional: true
 
   validates :patient, presence: true
   validates :doctor, presence: true
